69a37166c68a42971fde02a9271b746e1837ba66
josias <keassebojosias@gmail.com>
2025-12-09 16:21:23 +0000

    WIP: update env system handling

--- src/main/java/com/josias/planit/PlanitApplication.java
+++ src/main/java/com/josias/planit/PlanitApplication.java
@@ -6,21 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlanitApplication {
 
-	public static void main(String[] args) {
-        String profile = System.getProperty("spring.profiles.active", "dev");
-
-        // Load .env ONLY for local profiles
-        if (profile.equals("dev") || profile.equals("test")) {
-            io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.configure()
-                    .ignoreIfMalformed()
-                    .ignoreIfMissing()
-                    .load();
-
-            dotenv.entries().forEach(entry ->
-                    System.setProperty(entry.getKey(), entry.getValue()));
-        }
-
+    public static void main(String[] args) {
         SpringApplication.run(PlanitApplication.class, args);
-	}
+    }
 
 }
